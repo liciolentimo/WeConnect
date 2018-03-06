740f51e3ddf28a8621418153a468927b7da084e6
Licio Lentimo <lentimo@gmail.com>
2018-03-06 14:33:22 +0300

    Writes tests for register and duplicate [Starts #155737768]

--- api/v1/test_user.py
+++ api/v1/test_user.py
@@ -1,26 +1,40 @@
 import unittest
 from user import User,USERS
 from business import Business, businessdetails
+from app import app
+import json
 
 
 class UserTest(unittest.TestCase):
 
 	def setUp(self):
 		self.user = User()
+		self.test_app = app.test_client()
 		USERS = {'username':'john',
 				'email':'john@email.com',
 				'password':'123'	
 
 		}
+	def tearDown(self):
+		del self.user 	
 
 	def test_create_user(self):
+		response = self.test_app.post(
+					'/api/v1/register',
+					data=json.dumps(dict(
+						username='john',
+						email='john@email.com',
+						password='123')),
+					content_type='application/json')	
+		self.assertEqual(response.status_code,400)
+		self.assertIn('john',USERS,msg='user not found')
+
+	
+	def test_duplicate_users(self):
 		self.user.create_user('john','john@email.com','123')
-		self.assertIn('john',self.username)
-		self.assertIn('john@email.com',self.email)
-		self.assertIn('123',self.password)
+		msg = self.user.create_user('john','john@email.com','123')
+		self.assertIn("User already exists",msg)
 
-	# def test_get_user(self):
-
-			
+		
 if __name__ == '__main__':
 	unittest.main()
