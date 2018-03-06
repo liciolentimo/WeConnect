32907e9d5d05cc8660efc300170253e4bb961d01
Licio Lentimo <lentimo@gmail.com>
2018-03-06 10:46:19 +0300

    Adds business details list [Implements #155718008]

--- api/v1/business.py
+++ api/v1/business.py
@@ -1,19 +1,38 @@
 from flask import jsonify
-from user import User 
+from user import User,USERS 
+
+businessdetails = [{
+			'id': 1,
+			'name':'tuskys',
+			'location': 'nairobi',
+			'category':'supermarket'
+
+	},
+			{
+			'id':2,
+			'name':'andela',
+			'location':'nairobi',
+			'category':'tech'
+
+			}]
+
+			
 class Business():
 
+	
 	def __init__(self):
 		self.businessname = None
 		self.location = None
-		self.description = None
-		self.Business = {}
+		self.category = None
+	
 
-	def create_business(self,businessname,location,description):
+	def create_business(self,businessname,location,category):
 		business = {'businessname':businessname,
 					'location':location,
-					'description':description
+					'category':category
 		}
-		self.Business['business'] = business
+		businessdetails.append(business)
+		# self.Business['business'] = business
 		return business
 
 
