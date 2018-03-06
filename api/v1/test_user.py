f41200d50840837c2fb8d08c9ccc0ad7283b3e6f
Licio Lentimo <lentimo@gmail.com>
2018-03-06 20:13:30 +0300

    Adds get_user function test [Implements #155737768]

--- api/v1/test_user.py
+++ api/v1/test_user.py
@@ -1,6 +1,5 @@
 import unittest
 from user import User,USERS
-from business import Business, businessdetails
 from app import app
 import json
 
@@ -9,31 +8,55 @@ class UserTest(unittest.TestCase):
 
 	def setUp(self):
 		self.user = User()
-		self.test_app = app.test_client()
-		USERS = {'username':'john',
+		self.test = app.test_client()
+		self.creds = {'username':'john',
 				'email':'john@email.com',
 				'password':'123'	
 
 		}
 	def tearDown(self):
-		del self.user 	
+		del self.user 
 
-	def test_create_user(self):
-		response = self.test_app.post(
-					'/api/v1/register',
-					data=json.dumps(dict(
-						username='john',
-						email='john@email.com',
-						password='123')),
-					content_type='application/json')	
-		self.assertEqual(response.status_code,400)
-		self.assertIn('john',USERS,msg='user not found')
+	def test_register_user(self):
+		response = self.test.post(
+								'/api/v1/register',
+								data=json.dumps(self.creds),
+								headers={'Content-Type':'application/json'})
+		self.assertEqual(response.status_code,201)
+		self.assertIn('User successfully created',str(response.data))
+
+	def test_get_users(self):
+		response = self.test.get('/api/v1/users')
+		self.assertEqual(response.status_code,200)														
+
+
+	def login(self,email='john@email.com',password='123'):
+		users = {'email':email,'password':password}
+		return self.data.post('/api/v1/login',
+								headers={'Content-Type':'application/json'},
+								data = json.dumps(data))	
+
+	def test_cannot_login_unregistered_user(self):
+		result = self.test.post(
+								'/api/v1/login',
+								headers={'Content-Type':'application/json'},
+								data=json.dumps(self.creds))
+		self.assertEqual(result.status_code,401)							
 
 	
 	def test_duplicate_users(self):
 		self.user.create_user('john','john@email.com','123')
-		msg = self.user.create_user('john','john@email.com','123')
-		self.assertIn("User already exists",msg)
+		second_result = self.user.create_user('john','john@email.com','123')
+		result = json.loads(second_result.data.decode())
+		self.assertEqual(result['message'],'user already exists'+'please login')
+		self.assertEqual(second_result.status_code,202)
+
+	def test_invalid_email(self,email):
+		msg = ('john',self.email,'john@gmail.com')
+		self.assertEqual(msg,"enter a valid email address")
+
+	# def test_login_wrong_password(self):
+	# 	self.user.user()=[{'email':'john@email.com','password':'123'}]		
 
 		
 if __name__ == '__main__':
