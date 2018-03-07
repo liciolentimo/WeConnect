b21325f1653105dbdf98378e3048cdd505628ebd
Licio Lentimo <lentimo@gmail.com>
2018-03-07 19:18:05 +0300

    Adds create_user, login and reset_password functions [Delivers #155678604][Delivers #155678612][Delivers #155678824]

--- api/v1/user.py
+++ api/v1/user.py
@@ -1,45 +1,67 @@
-from flask import json
-
-USERS = [{
-			'username':'john',
-			'email':'john@email.com',
-			'password':'123'
-	}]
+import re
+# import regular expressions
 
 class User():
-	
-	
-	
-	
+
 	def __init__(self):
-		# super(User, self).__init__()
-		self.username = None
-		self.email = None
-		self.password = None
+		
+		self.list_user=[]
 		
 
-	def create_user(self,username,email,password):
-			user = {'username':username,
-					'email':email,
-					'password':password}
-			USERS.append(user)
-			return {'message': 'User successfully created'}
+	def create_user(self,username,email,password,confirm_password):
+			# initialize empty dictionary to store users
+			userinfo = {}
+			
+			for user in self.list_user:
+				if username == user['username']:
+					return "User already exists. Try again"
+				elif email == user['email']:
+					return "User already exists. Try again"	
+			# check for special characters in username
+			if not re.match("^[a-zA-Z0-9_]*$", username):
+				return "Invalid username."
+			elif len(password) < 6:
+				return "Your password should be at least 6 characters"
+			#check for valid email	
+			elif not re.match(r"(^[a-zA-Z0-9_.]+@[a-zA-Z0-9-]+\.[a-z]+$)", email):
+				return "Please enter a valid email address"	
+			#check whether password matches with confirm password
+			elif password == confirm_password:
+				userinfo['username'] = username	
+				userinfo['email'] = email
+				userinfo['password'] = password
+
+				self.list_user.append(userinfo)
+			else:
+				return "Passwords do not match"
+			return "User successfully registered"					
+				
+			
 			# self.Users['user'] = json.dumps(user)
 			return user		
 
 	def get_user(self):
-			return USERS
+			return list_user
 
 	def login(self,email,password):
-		user = [user for user in USERS if user['email']== email and user['password']==password]
-		return user
+		for user in self.list_user:
+			if email == user['email']:
+				if password == user['password']:
+					return "Login successful"
+				
+				return "Invalid username or password"
 
-	def reset_password(self,email,password,new_password):
-		user = [user for user in USERS if user['email']==email and user['password']==password]
-		user[0]['password']=new_password
-		return user
-	
-					
+		return "Account does not exist. Please register."				
+
+	def reset_password(self,new_password,confirm_password):
+		for user in self.list_user:
+			if new_password == confirm_password:
+				user['password'] = new_password
+				return "Password changed successfully"
+			
+			return "Passwords do not match"
+		return "User account does not exist"	
+				
 
 
 		# def find_element_by_name(self):
