994b7ff8cde9b0b3e8582cf86a5c23ee16d2737c
Licio Lentimo <lentimo@gmail.com>
2018-03-09 00:21:42 +0300

    Fixes session error in login function [Delivers #155678612]

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,13 +2,16 @@ from flask import Flask, url_for, request, abort, make_response,jsonify,session
 from user import User
 import json
 import os
-from business import Business,businessdetails
+from business import Business
+
 
 app = Flask(__name__)
 
+app.secret_key = os.urandom(24)
+
+
 users=User()
 business = Business()
-# app.SECRET_KEY = os.random(24)
 
 
 @app.route('/api/v1/register', methods=['POST'])
@@ -48,20 +51,16 @@ def get_users():
 
 @app.route('/api/v1/login',methods=['POST'])
 def login():
-	if not request.json:
-		abort(400)
-	data = request.get_json()
-	email = data.get('email')
-	password = data.get('password')
-	user = users.login(email,password)
-	# session['useremail'] = user['email']
-	if email not in session:
-		# if len(user)==0:
-		return jsonify({'status':'Not logged in'}),401
-	else:	
-	# session['useremail'] = user['email']
-	# user= session[email]
-		return jsonify({'logged_in':email}),202
+	# if not request.json:
+	# 	abort(400)
+	 if request.method == "POST":
+	 	email = request.json['email']
+	 	password = request.json['password']
+	 	session['email'] = email
+	 	res = users.login(email,password)
+	 	response = res
+	 	return res 
+	
 	
 @app.route('/api/v1/logout',methods=['POST'])
 def logout():
@@ -82,24 +81,34 @@ def reset_password():
 
 @app.route('/api/v1/addbusiness', methods=['GET','POST'])
 def register_business():
-	if request.method == 'POST':
-		if request.json:
-			data=request.get_json()
-			# email = data.get('email')
-			# password = data.get('password')
-			# user = users.create_user(email,password)
-			# user = users.login(email,password)
-			# logged_in_user = user['email']
-			# if logged_in_user:
-			# 	data = request.get_json()
-			# 	useremail = logged_in_user
-			name = data.get('name')
-			location = data.get('location')
-			category = data.get('category')
-			business.create_business(name,location,category)
-			return make_response(jsonify({'message':'business created successfully'}),201)
-		else:
-			return make_response(jsonify({'Business':businessdetails}),200)			
+	if session.get('username') is not None:
+		if request.method == 'POST':
+			businessname = request.json['name']
+			user = request.json['user']
+			location = request.json['location']
+			category = request.json['category']
+			result = business.create_business(businessname,user,location,category)
+			message = jsonify(result)
+			response.status_code = 201
+			return response	
+	
+		# if request.json:
+		# 	data=request.get_json()
+		# 	# email = data.get('email')
+		# 	# password = data.get('password')
+		# 	# user = users.create_user(email,password)
+		# 	# user = users.login(email,password)
+		# 	# logged_in_user = user['email']
+		# 	# if logged_in_user:
+		# 	# 	data = request.get_json()
+		# 	# 	useremail = logged_in_user
+		# 	name = data.get('name')
+		# 	location = data.get('location')
+		# 	category = data.get('category')
+		# 	business.create_business(name,location,category)
+		# 	return make_response(jsonify({'message':'business created successfully'}),201)
+		# else:
+		# 	return make_response(jsonify({'Business':businessdetails}),200)			
 
 
 
@@ -113,7 +122,7 @@ def business_details(business_id):
 	business = [business for business in businessdetails if business['id']== business_id]
 	if len(business) == 0:
 		return make_response(jsonify({'message':'business not found'}))
-		# abort(404)
+		abort(404)
 	return jsonify({'business':business[0]})	
 
 @app.route('/api/v1/updatebusiness',methods=['PUT'])
