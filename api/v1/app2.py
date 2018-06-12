73445e8f3f7dfbb72ea4d78b5a2ef3f7cf0eba4c
Licio Lentimo <lentimo@gmail.com>
2018-06-12 14:46:15 +0300

    Added useful imports

--- api/v1/app2.py
+++ api/v1/app2.py
@@ -1,5 +1,7 @@
-from flask import Flask 
+from flask import Flask, request, jsonify 
 from flask_sqlalchemy import SQLAlchemy
+import uuid
+from werkzeug.security import generate_password_hash, check password_hash
 
 app = Flask(__name__)
 
