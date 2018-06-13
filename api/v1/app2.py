df98659981b93881d2dc8bc39ecc7a55ff52369f
Licio Lentimo <lentimo@gmail.com>
2018-06-13 10:09:39 +0300

    Configured get_one_user route

--- api/v1/app2.py
+++ api/v1/app2.py
@@ -37,9 +37,18 @@ def get_all_users():
 		output.append(user_data)
 	return jsonify({'users' : output})
 
-@app.route('/user/<user_id>', methods=['GET'])
-def get_one_user():
-	return ''
+@app.route('/user/<public_id>', methods=['GET'])
+def get_one_user(public_id):
+	user = User.query.filter_by(public_id=public_id).first()
+	if not user:
+		return jsonify({'message' : 'No user found.'})
+
+	user_data = {}	
+	user_data['public_id'] = user.public_id
+	user_data['name'] = user.name
+	user_data['password'] = user.password
+	user_data['admin'] = user.admin
+	return jsonify({'user' : user_data})
 
 @app.route('/user', methods=['POST'])
 def create_user():
@@ -51,11 +60,11 @@ def create_user():
 
 	return jsonify({'message' : 'New user created!'})
 
-@app.route('/user/<user_id>', methods=['PUT'])
+@app.route('/user/<public_id>', methods=['PUT'])
 def promote_user():
 	return ''
 
-@app.route('/user/<user_id>', methods=['DELETE'])
+@app.route('/user/<public_id>', methods=['DELETE'])
 def delete_user():
 	return ''					
 
